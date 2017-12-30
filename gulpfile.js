4cc7ddab79a6f6f5ce645cbfbc8dafc96c9f1beb
Igor Marcossi <igormarcossi@gmail.com>
2017-12-30 18:09:51 -0200

    Development in progress. Trying to add form inputs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,77 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+var pump = require('pump')
+
+var scssInput = './src/scss/**/*.scss';
+var scssOutput = './dist/css';
+
+var jsInput = './src/js/**/*.js';
+var jsOutput = './dist/js/';
+
+var testOutput = './test';
+
+/////////////////////
+/////////CSS/////////
+/////////////////////
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var minify = require('gulp-clean-css');
-var rename = require('gulp-rename');
+var cleanCss = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var sassdoc = require('sassdoc');
 
-var input = './src/**/*.scss';
-var output = './dist/css';
-var testOutput = './test';
-
-gulp.task('sass', function () {
-  return gulp
-    .src(input)
-    .pipe(sourcemaps.init())
-    .pipe(sass()).on('error', onError)
-    .pipe(autoprefixer())
-    .pipe(gulp.dest(output))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(testOutput))
-    .pipe(minify())
-    .pipe(rename("ceres.min.css"))
-    .pipe(gulp.dest(output));
+gulp.task('sass', function (cb) {
+   pump([
+      gulp.src(scssInput),
+      sourcemaps.init(),
+      sass().on('error', onError),
+      autoprefixer(),
+      gulp.dest(scssOutput),
+      sourcemaps.write(),
+      gulp.dest(testOutput),
+      cleanCss(),
+      rename("ceres.min.css"),
+      gulp.dest(scssOutput),
+   ],cb);
 });
 
 gulp.task('sassdoc', function () {
-  return gulp
-    .src(input)
-    .pipe(sassdoc())
-    .resume();
+   return gulp
+   .src(scssInput)
+   .pipe(sassdoc())
+   .resume();
 });
 
-gulp.task('watch', function() {
-  return gulp
-    .watch(input, ['sass', 'sassdoc']);
+/////////////////////
+/////////JS//////////
+/////////////////////
+var uglify = require('gulp-uglify');
+var concatJS = require('gulp-concat');
+
+gulp.task('js', function (cb) {
+   pump([
+      gulp.src(jsInput),
+      concatJS('ceres.js'),
+      gulp.dest(jsOutput),
+      gulp.dest(testOutput),
+      uglify().on('error', onError),
+      rename("ceres.min.js"),
+      gulp.dest(jsOutput),
+   ],cb);
+});
+
+gulp.task('watchCSS', function() {
+   return gulp
+   .watch(scssInput, ['sass', 'sassdoc']);
+});
+
+gulp.task('watchJS', function() {
+   return gulp
+   .watch(jsInput, ['js']);
 });
 
 function onError(err) {
-  console.log(err);
-  this.emit('end');
+   console.log(err);
+   this.emit('end');
 }
 
-gulp.task('default', ['sass', 'sassdoc', 'watch']);
+gulp.task('default', ['js', 'sass', 'sassdoc', 'watchCSS', 'watchJS']);
