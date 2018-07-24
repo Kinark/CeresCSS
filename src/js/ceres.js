65406487e59e51e2de743fa99d4437d60bff3805
Kinark <igormarcossi@gmail.com>
2018-07-24 01:30:28 -0300

    Build

--- src/js/ceres.js
+++ src/js/ceres.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-  $('select').niceSelect();
+$(document).ready(function () {
+   $('select').niceSelect();
 });
 
-var $buoop = {notify:{i:-5},api:5}; 
-function $buo_f(){ 
- var e = document.createElement("script"); 
- e.src = "//browser-update.org/update.min.js"; 
- document.body.appendChild(e);
+var $buoop = { notify: { i: -5 }, api: 5 };
+function $buo_f() {
+   var e = document.createElement("script");
+   e.src = "//browser-update.org/update.min.js";
+   document.body.appendChild(e);
 };
-try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
-catch(e){window.attachEvent("onload", $buo_f)}
+try { document.addEventListener("DOMContentLoaded", $buo_f, false) }
+catch (e) { window.attachEvent("onload", $buo_f) }
